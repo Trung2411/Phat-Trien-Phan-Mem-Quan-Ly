bf6e2ae0159d080ba4093106cfc600670e603ed8
Your_UserNameTrung2411 <trunghoang3000@gmail.com>
2025-01-22 22:05:38 +0700

    HTML

---
+++ New_ProJect_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace New_ProJect_MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
